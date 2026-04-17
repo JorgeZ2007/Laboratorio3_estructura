ed85aa75ff6ae770dd09ce1d59d5f553c921cb2c
Your Name <you@example.com>
2026-04-17 18:58:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -98,8 +98,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
     while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
         pos++;
-        printf("%l", pos); 
-        if(is_equal(key, map->buckets[pos]->key) == 1)
+        if(is_equal(key, map->buckets[pos]->key) == 0)
             {
                 map->current = pos;
                 return map->buckets[pos];
